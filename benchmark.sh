a928499e4bbf9792c37ac1080e38d653146ba890
Jeff Bencin <jeff.bencin@gmail.com>
2023-07-14 17:24:03 -0400

    Add comments

--- benchmark.sh
+++ benchmark.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
+# Set up dependencies
 sudo apt-get install -y git build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 
+# Set up repository
 parent_dir="$HOME/git"
 mkdir -p "$parent_dir"
 cd "$parent_dir"
@@ -13,6 +15,7 @@ git clone https://github.com/hirosystems/stacks-subnets
 cd stacks-subnets
 git co benchmarking
 
+# Run benchmarks
 declare -a tests=( "test_15s_block" "test_max_block" "test_15s_block_stx_transfers_only" "test_max_block_stx_transfers_only" )
 for test in "${tests[@]}"; do
     logfile="$test.log"
